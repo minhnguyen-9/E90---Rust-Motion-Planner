64ef3750b0f4ab702df56b8ff54aa1f5578e7d03
minhnguyen-9 <ducminhtin3@gmail.com>
2023-02-14 13:57:14 -0500

    first working version - passes clippy

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,5 @@
-use geo::{Contains, EuclideanDistance, Intersects, Line, LineString, Point, Polygon};
-use plotters::prelude::*;
-
-// use geo::prelude::*;
-
-// use plotters::prelude::*;
-
-// fn main() {  # Plotters example
-//     let root_drawing_area = BitMapBackend::new("images/0.1.png", (1024, 768))
-//         .into_drawing_area();
-
-//     root_drawing_area.fill(&WHITE).unwrap();
-
-//     let mut chart = ChartBuilder::on(&root_drawing_area)
-//         .build_cartesian_2d(-3.14..3.14, -1.2..1.2)
-//         .unwrap();
-
-//     chart.draw_series(LineSeries::new(
-//         (-314..314).map(|x| x as f64 / 100.0).map(|x| (x, x.sin())),
-//         &RED
-//     )).unwrap();
-// }
+use geo::{Contains, Intersects, LineString, Point, Polygon};
+use plotters::{element::BackendCoordOnly, prelude::*};
 
 const MAX_ITERATIONS: usize = 1000;
 const SEGMENT_LENGTH: f64 = 0.1;
@@ -41,7 +21,7 @@ fn segment_is_free(
     start_coords: &Point<f64>,
     end_coords: &Point<f64>,
 ) -> bool {
-    let lstr = LineString::from(vec![start_coords.clone(), end_coords.clone()]);
+    let lstr = LineString::from(vec![*start_coords, *end_coords]);
 
     for p in obstacle_polygons {
         if p.intersects(&lstr) {
@@ -60,7 +40,7 @@ fn main() {
 
     let mut obstacle_polygons: Vec<Polygon<f64>> = Vec::new();
 
-    for (x0, y0, x1, y1) in rect_coords {
+    for (x0, y0, x1, y1) in rect_coords.iter().copied() {
         let points = vec![
             Point::new(x0, y0),
             Point::new(x1, y0),
@@ -92,10 +72,11 @@ fn main() {
     let mut tree_points = Vec::with_capacity(MAX_ITERATIONS);
     tree_points.push(q_init);
     let mut parents = Vec::with_capacity(MAX_ITERATIONS);
+    parents.push(0);
 
     let mut goal_idx = None;
 
-    for attempt in 0..MAX_ITERATIONS {
+    for _ in 0..MAX_ITERATIONS {
         // let mut q_rand: Point<f64>;
 
         let r: f64 = rand::random();
@@ -126,7 +107,7 @@ fn main() {
             // active_points[idx] + &diff[idx] * SEGMENT_LENGTH / dist[idx];
         };
 
-        if segment_is_free(&obstacle_polygons, &p_closest_to_rand, &q_new) {
+        if segment_is_free(&obstacle_polygons, p_closest_to_rand, &q_new) {
             tree_points.push(q_new);
             parents.push(idx);
             if q_new == q_goal {
@@ -141,59 +122,59 @@ fn main() {
 
     let mut chart_context = ChartBuilder::on(&root);
     chart_context.caption("Example Plot", ("sans-serif", 30));
-    let mut chart = chart_context.build_cartesian_2d(0.0..8.0f64, 0.0..8.0f64).unwrap();
+    let mut chart = chart_context
+        .build_cartesian_2d(0.0..8.0f64, 0.0..8.0f64)
+        .unwrap();
     // chart.configure_mesh().draw().unwrap();
 
-    // let obstacle_style = ShapeStyle {
-    //     color: BLUE.mix(0.6),
-    //     filled: true,
-    //     stroke_width: 2,
-    // };
+    let obstacle_style = ShapeStyle {
+        color: BLUE.mix(0.6),
+        filled: true,
+        stroke_width: 2,
+    };
 
-    // // Draw obstacle patches
-    // let patches: Vec<Rectangle<_>> = rect_coords
-    //     .iter()
-    //     .map(|(x_left, y_low, x_right, y_high)| {
-    //         Rectangle::new([(x_left, y_high), (x_right, y_low)], obstacle_style)
-    //     })
-    //     .collect();
+    // Draw obstacle patches
+    let patches: Vec<Rectangle<(f64, f64)>> = rect_coords
+        .iter()
+        .map(|(x_left, y_low, x_right, y_high)| {
+            Rectangle::new([(*x_left, *y_high), (*x_right, *y_low)], obstacle_style)
+        })
+        .collect();
 
-    // chart.draw_series(patches.iter()).unwrap();
+    chart
+        .draw_series::<BackendCoordOnly, Rectangle<(f64, f64)>, _, _>(patches.iter())
+        .unwrap();
 
-    // chart
-    //     .draw_series(
-    //         tree_points
-    //             .iter()
-    //             .map(|p| Circle::new((p.x(), p.y()), 3, &BLACK)),
-    //     )
-    //     .unwrap();
+    chart
+        .draw_series(
+            tree_points
+                .iter()
+                .map(|p| Circle::new((p.x(), p.y()), 3, &BLACK)),
+        )
+        .unwrap();
 
     for (point_idx, parent_idx) in parents.iter().enumerate() {
-        if *parent_idx >= 0 {
-            let x0 = tree_points[*parent_idx].x();
-            let y0 = tree_points[*parent_idx].y();
-            let x1 = tree_points[point_idx].x();
-            let y1 = tree_points[point_idx].y();
-            chart
-                .draw_series(LineSeries::new([(x1, y1), (x0, y0)], &RED))
-                .unwrap();
+        let x0 = tree_points[*parent_idx].x();
+        let y0 = tree_points[*parent_idx].y();
+        let x1 = tree_points[point_idx].x();
+        let y1 = tree_points[point_idx].y();
+        chart
+            .draw_series(LineSeries::new([(x0, y0), (x1, y1)], &YELLOW))
+            .unwrap();
+    }
+
+    if let Some(mut current_idx) = goal_idx {
+        let mut path = vec![];
+        while current_idx != 0 {
+            let p = tree_points[current_idx];
+            path.push((p.x(), p.y()));
+            current_idx = parents[current_idx];
         }
+        let p = tree_points[current_idx];
+        path.push((p.x(), p.y()));
+
+        let example = LineSeries::new(path.iter().map(|p| (p.0, p.1)), &BLUE);
+        chart.draw_series(example).unwrap();
     }
-
-    // if let Some(goal_idx) = goal_idx {
-    //     let path_points = (0..)
-    //         .map(|idx| {
-    //             let i = goal_idx;
-    //             let p = tree_points[i];
-    //             (p.x(), p.y())
-    //         })
-    //         .take_while(|_| goal_idx >= 0)
-    //         .collect::<Vec<_>>();
-
-    //     let example = LineSeries::new(path_points.iter().map(|p| (p.0, p.1)), &GREEN);
-    //     chart.draw_series(example).unwrap();
-
     chart.configure_mesh().draw().unwrap();
-
-    }
-
+}
